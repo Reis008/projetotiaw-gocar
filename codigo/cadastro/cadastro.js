ff76084e47f895a05ed449a3dcfad8f9fdf80627
Gabriel Luís <77586106+Dr4gulaa@users.noreply.github.com>
2022-11-20 01:59:46 -0300

    Cadastro V 0.2
    - Validação de confirmação de senhas
    - LocalStorage cadastro
    - Limpeza de formulário após cadastro

--- codigo/cadastro/cadastro.js
+++ codigo/cadastro/cadastro.js
@@ -1,15 +1,74 @@
 
-        function mesmasenha(){
-            const senha = document.querySelector('input[name=senha]');
-            const confirmar = document.querySelector('input[name=confirmar]');
+/*Validação de senhas*/
+function validatesenha(){
+    var senha = document.getElementById("senha");
+    var confirm_senha = document.getElementById("confirmsenha");
 
-            if (confirmar.value === senha.value){
-                confirmar.setCustomValidity('');
-            }else{
-                confirmar.setCustomValidity('As senhas não conferem');
-            }
+    if(senha.value != confirm_senha.value) {
+        confirm_senha.setCustomValidity(alert("Senhas diferentes"));
+    } else {
+        confirm_senha.setCustomValidity(alert("Cadastrado com sucesso"));
+        cadastroNovo();
+        limparform();
+    }
+
+    senha.onchange = validatesenha;
+    confirm_senha.onkeyup = validatesenha;
+}
+
+/*Vetor cadastros*/
+let bancoContatos = JSON.parse (    localStorage.getItem("cadastros")   );
+
+if(!bancoContatos){
+ bancoContatos = [
+        {
+            "nomeusuario": "Jorge",
+            "emailusuario": "jorgecampos@hotmail.com",
+            "telefoneusuario": "(31) 99845 - 5643",
+            "cpfusuario": "123.456.789 - 00",
+            "senha" : "coxinha123"
+        },
+        {
+            "nomeusuario": "Maria",
+            "emailusuario": "mariacampos@hotmail.com",
+            "telefoneusuario": "(31) 99845 - 5643",
+            "cpfusuario": "123.456.789 - 00",
+            "senha" : "coxinha321"
         }
-    function senhaOK(){ alert("Cadastro Aprovado!") }
+    ]
+}
+
+/*Função Limpar formulário*/
+function limparform(){
+    document.getElementById("nome").value="";
+    document.getElementById("email").value="";
+    document.getElementById("tel").value="";
+    document.getElementById("cpf").value="";
+    document.getElementById("senha").value="";
+
+}
 
 
-   
+/*Função Cadastro*/
+function cadastroNovo(){
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var tel = document.getElementById("tel").value;
+    var cpf = document.getElementById("cpf").value;
+    var senha = document.getElementById("senha").value;
+
+    var novoCadastro = {
+        "nomeusuario": nome,
+        "emailusuario": email,
+        "telefoneusuario": tel,
+        "cpfusuario": cpf,
+        "senha" : senha
+    }
+
+    bancoContatos.push(novoCadastro);
+    localStorage.setItem("cadastros",   JSON.stringify(bancoContatos));
+    console.log(bancoContatos);
+
+}
+
+
